b72397949426be13a580f254a2f996bdc82be7e1
Shogo-Nakasaki <nsahkoagsoaki@gmail.com>
2021-04-08 11:43:48 +0900

    ToDoリストのプロジェクトの作成と簡単なレイアウト作成を行なった。

---
+++ MyTODO/MyTODO/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MyTODO
+//
+//  Created by hb19a068 on 2021/04/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addBtnAction(_ sender: Any) {
+    }
+    
+}
+
