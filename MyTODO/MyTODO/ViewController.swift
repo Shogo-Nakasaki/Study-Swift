1bfa357302b7ddcc09c85ca443e3cbff97dcceb4
Shogo-Nakasaki <nsahkoagsoaki@gmail.com>
2021-04-10 17:38:52 +0900

    エミュレーターでチェックするためにViewControllerのコードを変更・追加

--- MyTODO/MyTODO/ViewController.swift
+++ MyTODO/MyTODO/ViewController.swift
@@ -7,16 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// 変更：プロトコルの追加
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     
+    // 追加：変数todoListの追加
+    var todoList = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
     @IBAction func addBtnAction(_ sender: Any) {
     }
+    // 追加：セルの数を指定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoList.count
+    }
     
+    // 追加：セルの中身の設定
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
+        let todoTitle = todoList[indexPath.row]
+        cell.textLabel?.text = todoTitle
+        return cell
+    }
 }
-
