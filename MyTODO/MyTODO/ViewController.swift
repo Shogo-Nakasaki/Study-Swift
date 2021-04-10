5ff2327614b13e9a52feba3c51b0a4fefcac6534
Shogo-Nakasaki <nsahkoagsoaki@gmail.com>
2021-04-10 17:49:03 +0900

    追加：ToDoリストに記入する機能

--- MyTODO/MyTODO/ViewController.swift
+++ MyTODO/MyTODO/ViewController.swift
@@ -17,8 +17,24 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // 追加：ボタンをタップするとテキストフィールド付きのアラートを出す
     @IBAction func addBtnAction(_ sender: Any) {
+        let alertController = UIAlertController(title: "ToDo追加", message: "ToDoを入力してください。", preferredStyle: UIAlertController.Style.alert)
+        alertController.addTextField(configurationHandler: nil)
+        let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { (acrion: UIAlertAction) in
+            // 追加：OKをタップした時の処理
+            if let textField = alertController.textFields?.first {
+                self.todoList.insert(textField.text!, at: 0)
+                self.tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: UITableView.RowAnimation.right)
+            }
+        }
+        alertController.addAction(okAction)
+        let cancelButton = UIAlertAction(title: "CANCEL", style: UIAlertAction.Style.cancel, handler: nil)
+        alertController.addAction(cancelButton)
+        present(alertController, animated: true, completion: nil)
     }
+    
     // 追加：セルの数を指定
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return todoList.count
